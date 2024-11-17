c5bfc93f0b3b30b7327a68add4079557ae2649c2
Andysword <andrewhuang2019@gmail.com>
2024-11-17 11:51:45 -0600

    Deleting comments

--- hackutd24/src/app/components/pythonscript.tsx
+++ hackutd24/src/app/components/pythonscript.tsx
@@ -2,22 +2,22 @@
 
 export const callPythonScript = async () => {
   try {
-    // Making a POST request to the /api/run-python route
+    
     const response = await fetch('../../api/pythonapi/route.tsx', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         scriptPath: '/Users/andrewhuang/Desktop/Projects and Fun/Programming/Hackathon/hackutd-2024/hackutd24/hackutd24/tools/data_processing.py',  // The full path to the script on your server
-        args: ['arg1', 'arg2'], // Any arguments your script might need
+        args: ['arg1', 'arg2'], 
       }),
     });
 
-    // Handle the response from the server
+   
     if (!response.ok) {
       throw new Error(`Failed to call API: ${response.statusText}`);
     }
 
-    // Parse the JSON response
+ 
     const data = await response.json();
     console.log('Python script output:', data.output);
   } catch (error) {
