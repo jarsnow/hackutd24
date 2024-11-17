8d9a1a74ccad256bd6493fe23f10f8e5f9f4f7f5
Cooper Wright <73917610+CooperW12@users.noreply.github.com>
2024-11-17 02:03:54 -0600

    report page but beavers 🦫

--- hackutd24/src/app/components/report.tsx
+++ hackutd24/src/app/components/report.tsx
@@ -7,11 +7,20 @@ const Report: React.FC = () => {
 
     return (
 
-        <div className={`${isReportVisible ? "block" : "hidden"} flex-1 bg-gray-100 p-4`}>
+        <div className={`${isReportVisible ? "block" : "hidden"} flex-1 bg-gray-100`}>
 
                 <p className="text-2xl font-bold">Report</p>
 
                 <p>Get Started</p>
+
+                <div className='flex flex-wrap justify-evenly'>
+                    <img className='border-4 rounded-xl m-8 w-1/2' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                    <img className='border-4 rounded-xl m-8 w-1/3' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                    <img className='border-4 rounded-xl m-4 w-2/5' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                    <img className='border-4 rounded-xl m-8 w-2/5' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                    <img className='border-4 rounded-xl m-4 w-1/4' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                    <img className='border-4 rounded-xl m-8 w-1/4' src='https://ichef.bbci.co.uk/images/ic/1920xn/p0k3rf8d.jpg.webp'></img>
+                </div>
 
         </div>
     );
