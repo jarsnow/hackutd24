e1f34f34a01337fc8cbbeab0e9e586822ef7feac
Cooper Wright <73917610+CooperW12@users.noreply.github.com>
2024-11-17 06:32:52 -0600

    styling

--- hackutd24/src/app/components/title.tsx
+++ hackutd24/src/app/components/title.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Image from 'next/image';
+import logo from './/../logo.png'
 import React from 'react'
 import { useVisibilityContext } from "./visibilitycontext";
 
@@ -8,12 +10,13 @@ const Title: React.FC = () => {
     return (
 
         <div className={`${isTitleVisible ? "block" : "hidden"} 
-        w-full flex-1 bg-gray-100 p-4 flex justify-center items-center h-full`}>
+        w-full flex-1 bg-dark p-4 flex justify-center items-center h-full flex-col`}>
+            <div className='border border-8 rounded-xl border-white border-double'>
 
-                <p className="animate-fadein text-2xl font-bold text-6xl">
-                    Watt Wise
-                </p>
+            <Image src={logo} width={500} height={500} alt='logo'/>
 
+            </div>
+            <p className='text-white m-4'>Developed for HackUTD 2024 by Andrew Huang, John Rader, and Cooper Wright</p>
         </div>
     );
 
